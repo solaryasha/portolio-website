443d5ceada46ff24475b53cad95983331a72aac2
Volodymyr Pavlenko <solaryasha@gmail.com>
2025-03-31 11:03:08 -0400

    add about page mockup

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -3,16 +3,52 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar"
+
+import { User, Home } from "lucide-react"
+
+
+const navigationItems = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home
+  },
+  {
+    title: "About me",
+    url: "/about",
+    icon: User
+  },
+]
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Volodymyr Pavlenko</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {navigationItems.map(item => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
